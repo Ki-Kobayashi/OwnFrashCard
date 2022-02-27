92a263ee17d150fe1d5d5f8ca15fd312dbec5867
DESKTOP-4NN0BLD\gkkin <baz.fez.ad@gmail.com>
2022-02-27 22:33:20 +0900

    設定ファイルの反映（ignore/gradle）

--- app/src/main/java/com/example/ownfrashcard/MainActivity.kt
+++ app/src/main/java/com/example/ownfrashcard/MainActivity.kt
@@ -2,21 +2,71 @@ package com.example.ownfrashcard
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+        private val TAG = this::class.java.simpleName
+        private var mBgColorCode = 0
+
+
         override fun onCreate(savedInstanceState: Bundle?) {
                 super.onCreate(savedInstanceState)
                 setContentView(R.layout.activity_main)
+
         }
 
+        override fun onResume() {
+                super.onResume()
+        }
+
+        override fun onPause() {
+                super.onPause()
+        }
+
+        /**
+         * Viewの初期化
+         */
         private fun initView() {
                 imageButtonTest.setOnClickListener { }
-                imageButtonWordEdit.setOnClickListener { }
+                imageButtonWordEdit.setOnClickListener { clickWordEditBtn() }
+
+                buttonBgDark.setOnClickListener(this)
+                buttonBgDarkBlue.setOnClickListener(this)
+                buttonBgWhite.setOnClickListener(this)
+                buttonBgPink.setOnClickListener(this)
+                buttonBgGreen.setOnClickListener(this)
+                buttonBgYellow.setOnClickListener(this)
         }
 
-        private fun clickTextBtn() {
-                val intent = Intent(this::)
+
+        private fun clickTestBtn() {
+//                val intent = Intent(this@MainActivity, )
+        }
+
+        private fun clickWordEditBtn() {
+                // ★TODO:applyを使用してスッキリと書く
+                val intent = Intent(this, EditActivity::class.java).apply {
+//                        putExtra(Consts.IntentKey.KEY_BG_COLOR, "someting")
+                }
+                startActivity(intent)
+        }
+
+        override fun onClick(v: View) {
+//                TODO:背景色を変える処理
+                val  btn = v as Button
+                var intBgColor = 0
+                when (btn) {
+                        buttonBgDarkBlue -> intBgColor = R.color.bg_blue
+                        buttonBgDark -> intBgColor = R.color.bg_dark
+                        buttonBgWhite -> intBgColor = R.color.bg_white
+                        buttonBgPink -> intBgColor = R.color.bg_pink
+                        buttonBgGreen -> intBgColor = R.color.bg_green
+                        buttonBgYellow -> intBgColor = R.color.bg_yellow
+                }
+                ConstraintLayoutMain.setBackgroundResource(intBgColor)
+//                mBgColorCode = p0.background.
         }
 }
---
+++ app/src/main/java/com/example/ownfrashcard/const/Consts.kt
@@ -0,0 +1,11 @@
+package com.example.ownfrashcard.const
+
+/**
+ * Created by K.Kobayashi on 2022/02/25.
+ */
+object Consts {
+        object IntentKey {
+                const val KEY_EDIT_STATUS = "key_edit_add_status_code"
+
+        }
+}
