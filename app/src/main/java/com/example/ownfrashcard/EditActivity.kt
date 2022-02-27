ba1479e660e0407ed8b08bb9f8ac66cee0ede4ae
DESKTOP-4NN0BLD\gkkin <baz.fez.ad@gmail.com>
2022-02-28 06:07:13 +0900

    ああ

--- app/src/main/java/com/example/ownfrashcard/EditActivity.kt
+++ app/src/main/java/com/example/ownfrashcard/EditActivity.kt
@@ -1,12 +1,53 @@
 package com.example.ownfrashcard
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ownfrashcard.const.Consts
+import kotlinx.android.synthetic.main.activity_edit.*
 
 class EditActivity : AppCompatActivity() {
+        private val TAG = EditActivity::class.java.simpleName
+        private var mStatus = ""
+
         override fun onCreate(savedInstanceState: Bundle?) {
                 super.onCreate(savedInstanceState)
                 setContentView(R.layout.activity_edit)
-//                わーい
+
+                initView()
         }
+
+        override fun onResume() {
+                super.onResume()
+        }
+
+        override fun onPause() {
+                super.onPause()
+        }
+
+        private fun initView() {
+                ConstraintLayoutEdit.setBackgroundResource(bgColorCode)
+
+                mStatus = intent.extras!!.getString(Consts.IntentKey.KEY_EDIT_STATUS)!!
+                textViewStatus.text = mStatus
+
+                imageButtonRegister.setOnClickListener{
+                        if (mStatus == getString(R.string.title_add_new_word)){
+                                addNewWord()
+                        } else{
+                                changeWord()
+                        }
+
+                }
+        }
+
+        private fun changeWord() {
+
+        }
+
+        private fun addNewWord() {
+
+        }
+
+
 }
