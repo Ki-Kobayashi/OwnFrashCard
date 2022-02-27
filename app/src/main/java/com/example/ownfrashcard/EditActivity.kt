3b2771cb01b40e3aa3602f513963e93ddfde5834
DESKTOP-4NN0BLD\gkkin <baz.fez.ad@gmail.com>
2022-02-28 01:18:18 +0900

    わーいテスト

--- app/src/main/java/com/example/ownfrashcard/EditActivity.kt
+++ app/src/main/java/com/example/ownfrashcard/EditActivity.kt
@@ -7,5 +7,6 @@ class EditActivity : AppCompatActivity() {
         override fun onCreate(savedInstanceState: Bundle?) {
                 super.onCreate(savedInstanceState)
                 setContentView(R.layout.activity_edit)
+//                わーい
         }
 }
